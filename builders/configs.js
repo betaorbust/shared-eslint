8ec74cdee5bf511edd2b14cdf96344a0c9e70a70
Jacques Favreau <betaorbust@users.noreply.github.com>
2020-08-15 15:48:25 -0700

    feat: Allow reduce in javascript/typescript.

--- builders/configs.js
+++ builders/configs.js
@@ -33,6 +33,9 @@ const someGoodRules = {
 	// I love this idea, but in practice it catches too many legit names
 	// like devDeps, pkg instead of the reserved "package" etc.
 	'unicorn/prevent-abbreviations': 'off',
+	// I like the idea, but reduce is just so dang useful to me.
+	// Might change in the future, but for now, turning it off.
+	'unicorn/no-reduce': 'off',
 	// Yeah, don't do that
 	'no-console': 'error',
 	// Always space your comments
