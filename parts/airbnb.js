0a06e05a727fccee3b056b3f790ededc7664289e
Jacques Favreau <betaorbust@users.noreply.github.com>
2020-12-28 00:08:02 -0800

    fix: Move 'react/require-default-props' to airbnb-typescript.

--- parts/airbnb.js
+++ parts/airbnb.js
@@ -5,9 +5,5 @@ module.exports = {
 		...baseRules,
 		'import/no-default-export': 'off',
 		'import/prefer-default-export': 'error',
-		// I like the idea, but between not using null and stricter types,
-		// I just keep fighting this one over and over. If we disable the no-null
-		// rule then we should turn this on.
-		'react/require-default-props': 'off',
 	},
 };
